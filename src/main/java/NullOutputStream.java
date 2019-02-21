e794e75add5db7ecddb0aaf36a80b19da1196561
Adelaiten <karoltrzaska19@gmail.com>
2019-02-21 13:00:14 +0100

    add to hashtable

---
+++ src/main/java/NullOutputStream.java
@@ -0,0 +1,19 @@
+import java.io.OutputStream;
+
+public class NullOutputStream
+extends OutputStream {
+    public void close() {
+    }
+
+    public void flush() {
+    }
+
+    public void write(byte[] arrby) {
+    }
+
+    public void write(byte[] arrby, int n, int n2) {
+    }
+
+    public void write(int n) {
+    }
+}
---
+++ src/main/java/WordList.java
@@ -0,0 +1,24 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+
+public class WordList {
+    private final HashTable hashTable;
+
+    public WordList(String fileName, StringHasher stringHasher) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(fileName));
+        int n = Integer.parseInt(bufferedReader.readLine());
+        this.hashTable = new HashTable((int)((double)n * 1.2), stringHasher);
+
+        for (int i = 0; i < n; ++i) {
+            this.hashTable.add(bufferedReader.readLine().trim().toUpperCase());
+        }
+        bufferedReader.close();
+    }
+
+    public boolean lookup(String string) {
+        return this.hashTable.lookup(string.toUpperCase());
+    }
+}
