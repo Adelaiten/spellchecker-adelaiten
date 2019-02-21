4f0ec83eb4ab484f2e9675c9f94a6d3e1e844f29
Adelaiten <karoltrzaska19@gmail.com>
2019-02-21 19:25:53 +0100

    test hash table add method

--- src/test/java/HashTableTest.java
+++ src/test/java/HashTableTest.java
@@ -1,6 +1,24 @@
+import hashers.BetterStringHasher;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 
 public class HashTableTest {
 
+    @Test
+    public void testAddMethod() {
+        BetterStringHasher betterStringHasher = new BetterStringHasher();
+        HashTable hashTable = new HashTable(100, betterStringHasher);
+        hashTable.add("test");
+        hashTable.add("smash");
+        hashTable.add("blank");
+        Assertions.assertAll(
+                () -> Assertions.assertTrue(hashTable.lookup("test")),
+                () -> Assertions.assertTrue(hashTable.lookup("smash")),
+                () -> Assertions.assertTrue(hashTable.lookup("blank")),
+                () -> Assertions.assertFalse(hashTable.lookup("notexist"))
+        );
+    }
+
+    
 
 }
